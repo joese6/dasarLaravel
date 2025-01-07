ad29dea5f34f0d137a1171c205f506d541a92dad
Johan/joese6 <joese6@gmail.com>
2025-01-07 09:30:23 +0700

    perubahan

--- resources/views/sesi/welcome.blade.php
+++ resources/views/sesi/welcome.blade.php
@@ -1,10 +1,9 @@
 @extends('layout/index')
 @section('konten')
-<div class="w-50 text-center border rounded px-3 py-3 mx-auto">
-    <h1>Selamat Datang</h1>
-    <p>Silahkan LOGIN atau REGISTER untuk mengakses Data Siswa dan Kelas</p>
-    <a href="/sesi" class="btn btn-primary btn-lg">LOGIN</a>
-    <a href="/sesi/register" class="btn btn-success btn-lg">REGISTER</a>
-</div>
+    <div class="w-50 text-center border rounded px-3 py-3 mx-auto">
+        <h1>Selamat Datang</h1>
+        <p>Silahkan LOGIN atau REGISTER untuk mengakses Data Siswa dan Kelas!</p>
+        <a href="/sesi" class="btn btn-primary btn-lg">LOGIN</a>
+        <a href="/sesi/register" class="btn btn-success btn-lg">REGISTER</a>
+    </div>
 @endsection
-
