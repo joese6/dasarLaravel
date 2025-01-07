221d1341660eed396ab428b3006a8390c48a1e07
Johan/joese6 <joese6@gmail.com>
2025-01-07 09:36:34 +0700

    projek crud dasar laravel 11

--- resources/views/sesi/welcome.blade.php
+++ resources/views/sesi/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('konten')
     <div class="w-50 text-center border rounded px-3 py-3 mx-auto">
         <h1>Selamat Datang</h1>
-        <p>Silahkan LOGIN atau REGISTER untuk mengakses Data Siswa dan Kelas!</p>
+        <p>Silahkan LOGIN atau REGISTER untuk mengakses Data Siswa dan Kelas</p>
         <a href="/sesi" class="btn btn-primary btn-lg">LOGIN</a>
         <a href="/sesi/register" class="btn btn-success btn-lg">REGISTER</a>
     </div>
